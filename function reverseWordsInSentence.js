c26b4b3789db8e14b640e8f4ebe49155fe17dbf6
karthik885 <93860316+karthik885@users.noreply.github.com>
2023-10-13 18:55:45 +0530

    Add files via upload

---
+++ function reverseWordsInSentence.js
@@ -0,0 +1,12 @@
+function reverseWordsInSentence(sentence) {
+    const words = sentence.split(' ');
+    const reversedWords = words.map(word => {
+        return word.split('').reverse().join('');
+    });
+    const reversedSentence = reversedWords.join(' ');
+
+    return reversedSentence;
+}
+const inputSentence = "Hello, world!";
+const reversedSentence = reverseWordsInSentence(inputSentence);
+console.log(reversedSentence);
---
+++ sorting.js
@@ -0,0 +1,4 @@
+const array = [5, 2, 9, 1, 5, 6];
+array.sort((a, b) => b - a);
+
+console.log(array); 
